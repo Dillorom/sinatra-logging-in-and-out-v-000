7c88059edceea80f6a8aacf977e123e10cae3ec0
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-10 16:44:33 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     @user = User.find(params[:id])
     session[:id] = @user.id
     @user.id
   end
 
-  def is_logged_in? 
+  def self.is_logged_in? 
   end
 end
