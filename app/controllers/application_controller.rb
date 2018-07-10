e840f4bd94a11727014edff3b23d2dbf7605dcf3
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-10 17:49:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username], password: params[:password])
     if
       session[:user_id] = @user.id
-      redirect '/account'
+      redirect to '/account'
     elsif
       @user == nil
-        redirect '/error'
+        redirect to '/error'
     end
   end
 
